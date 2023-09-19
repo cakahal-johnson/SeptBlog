f488562a8d7f1155765e17e4bc429556bc9619ef
Akahal Vitalis <akahal.info@gmail.com>
2023-09-19 15:29:58 +0100

    first commit

---
+++ config/constants.php
@@ -0,0 +1,11 @@
+<?php
+
+define("IN_DIR", realpath(dirname(__FILE__)));
+
+define('ROOT_URL', 'http://localhost/SeptBlog/');
+
+// ROOT_URL The name of the constant. Note : It is possible to define() constants with reserved or even invalid names, whose value can (only) be retrieved with constant(). However, doing so is not recommended.
+
+//realpath() expands all symbolic links and resolves references to /./, /../ and extra / characters in the input path and returns the canonicalized absolute pathname.
+
+?>
---
+++ templates/nav.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . '/../config/constants.php';
+
+//const string __DIR__ = "c:\\wamp64\\www\\SeptBlog\\templates"
+// The directory of the file. If used inside an include, the directory of the included file is returned. This is equivalent to dirname(__FILE__). This directory name does not have a trailing slash unless it is the root directory. (Added in PHP 5.3.0.)
+
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.php">About</a></li>
+                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.php">Sample Post</a></li>
+                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
